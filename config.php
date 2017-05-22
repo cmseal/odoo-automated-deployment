1a710ae2f2df949770d8fccd75d19843c007cd79
Chris Seal <chris.seal@me.com>
2017-05-22 17:07:02 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+ // change this path to point to the checked out version of the odoo_mobile git repo
+ define('ODOO_MOBILE_PATH', './../../odoo_mobile/' );
+ // change this the URL of the Odoo instance
+ define('ODOO_URL', 'http://localhost:8069' );
+ define('ODOO_DB', 'odoo' );
+ define('ODOO_SESSION_LIFETIME', 30 );
+ define('ODOO_CON_FACTORY','OdooConJsonRpc');
