f746fc1ff719877f511490747885243ecff2c5cb
Chris Seal <chris.seal@me.com>
2017-05-25 12:19:24 +0100

    Update odoo_first_login.sh

--- odoo_first_login.sh
+++ odoo_first_login.sh
@@ -2,15 +2,20 @@ echo "Remove setup cron job"
 touch cron-empty
 crontab -u odoo cron-empty
 
+##TODO:
+# Remove ForceCommand line from /etc/ssh/sshd_config
+
+echo "Clone Odoo from git"
+cd ~
+git clone https://github.com/mcb30/odoo.git --depth 20 --branch import
+
 echo "Clone project from git"
 cd ~
-##TODO:
 git clone "$unipart_username"@git.unipart.io:/home/scm/"$project_repo_name".git
 
 if test "$mobile_project_repo_name" != ""; then
 	echo "Clone mobile from git"
-	cd /home/odoo/
-	##TODO:
+	cd ~
 	git clone "$unipart_username"@git.unipart.io:/home/scm/"$mobile_project_repo_name".git
 fi
 
@@ -43,6 +48,44 @@ if wkhtmltopdf --help | grep -q "Reduced Functionality"; then
    sudo dpkg -i /tmp/wkhtmltox-0.12.2_linux-trusty-amd64.deb
    sudo rm /tmp/wkhtmltox-0.12.2_linux-trusty-amd64.deb
 fi
+
+echo "Setup Apache mods"
+sudo /etc/init.d/apache2 start
+sudo update-rc.d apache2 defaults
+sudo a2enmod proxy
+sudo a2enmod proxy_http
+sudo a2enmod proxy_ajp
+sudo a2enmod rewrite
+sudo a2enmod deflate
+sudo a2enmod headers
+sudo a2enmod proxy_balancer
+sudo a2enmod proxy_connect
+sudo a2enmod proxy_html
+
+echo "Create and amend Apache .conf files"
+sudo mv vm_ip_address.conf /etc/apache2/sites-available/"$vm_ip_address".conf
+sudo mv vm_ip_address-mobile.conf /etc/apache2/sites-available/"$vm_ip_address"-mobile.conf
+sudo sed -i -e 's/vm_ip_address/'"$vm_ip_address"'/g' /etc/apache2/sites-available/"$vm_ip_address".conf
+sudo sed -i -e 's/vm_ip_address/'"$vm_ip_address"'/g' /etc/apache2/sites-available/"$vm_ip_address"-mobile.conf
+
+echo "Remove default Apache index and replace with redirect (just incase)"
+sudo rm /var/www/html/index.html
+sudo mv ~/index.html /var/www/html/index.html
+sudo sed -i -e 's/vm_ip_address/'"$vm_ip_address"'/g' /var/www/html/index.html
+
+echo "Make host directories to keep Apache happy"
+sudo mkdir /var/www/vhosts
+sudo mkdir /var/www/vhosts/"$vm_ip_address"
+sudo mkdir /var/www/vhosts/"$vm_ip_address"/httpdocs
+
+echo "Enable new Apache sites"
+sudo a2ensite "$vm_ip_address"
+sudo a2ensite "$vm_ip_address"-mobile
+sudo a2dissite 000-default
+
+## TODO: setup script as odoo service
+echo "Make ./run_odoo.sh script"
+sudo chmod +x ~/run_odoo.sh
 
 echo "Update mobile config.php file"
 sudo mv ~/config.php ~/mclaren/mobile/config.php
